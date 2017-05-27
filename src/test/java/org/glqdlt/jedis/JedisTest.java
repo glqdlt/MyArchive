1322b209faa90b8f4753a7054b7abe186646af84
glqdlt <glqdlt@DESKTOP-JCAGAG7>
2017-05-27 13:52:06 +0900

    new add Gson.dependency
    jedis 테스트코드 강화

--- src/test/java/org/glqdlt/jedis/JedisTest.java
+++ src/test/java/org/glqdlt/jedis/JedisTest.java
@@ -1,6 +1,9 @@
 package org.glqdlt.jedis;
 
+import org.glqdlt.home.servers.domain.user.UserVO;
 import org.junit.Test;
+
+import com.google.gson.Gson;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -11,12 +14,23 @@ public class JedisTest {
 	@Test
 	public void connectionTest() {
 		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-		JedisPool pool = new JedisPool(jedisPoolConfig, "host주소", 16379, 1000, "패스워드");
+		JedisPool pool = new JedisPool(jedisPoolConfig, "url", 16379, 1000, "pw");
 		// config, host주소, 포트, 타임아웃, 패스워드
 		Jedis jedis = pool.getResource();
-
-		// jedis.set("foo", "bar");
-		String val = jedis.get("foo");
+		UserVO uvo = new UserVO();
+		uvo.setUserId("testid");
+		uvo.setUserPw("testpw");
+		uvo.setUserEmail("test@test");
+		uvo.setUserAuth(9);
+		
+		
+		Gson gson = new Gson();
+		String uvoStr = gson.toJson(uvo);
+//		jedis.set("session1", uvoStr);
+		
+//		 jedis.set("foo", "bar1");
+//		 jedis.del("foo");
+		String val = jedis.get("session1");
 
 		System.out.println(val);
 
