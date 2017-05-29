7b0fdf7e9e8b02493e6695fa41dc015bb8a846af
IGLOO <IGLOO@IGLOO-PC>
2017-05-29 16:38:34 +0900

    new add zipEx
    집악축 추가
    
    꼮 zipEntry.setTime(0)으로 해서 위조여부를 없애줘서 hash를 같게 처리할것.

--- src/main/java/org/glqdlt/home/utills/PathFinder.java
+++ src/main/java/org/glqdlt/home/utills/PathFinder.java
@@ -1,9 +1,24 @@
 package org.glqdlt.home.utills;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
+
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
+import org.apache.commons.io.IOUtils;
 
 public class PathFinder {
 
@@ -17,53 +32,184 @@ public class PathFinder {
 
 		File target = new File(path);
 		int childCount = 1;
+		int imgCheck = 0;
+
+		String childName = "";
+		String childAbsolutePath = "";
+		String[] fileInformation;
 
 		for (File childFile : target.listFiles()) {
+			boolean isLoop = false;
+
+			childName = childFile.getName();
+			childAbsolutePath = childFile.getAbsolutePath();
+
 			System.out.println("count:[" + parentCount + "-" + childCount + "]");
+			System.out.println("imgCheck: " + imgCheck);
+
+			if (imgCheck >= (target.listFiles().length / 2)) {
+
+				System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+				System.out.println(path);
+				System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+
+			}
+
 			if (childFile.isFile()) {
-				System.out.println("file name: " + childFile.getName());
-				System.out.println("file absolute: " + childFile.getAbsolutePath());
-				System.out.println("file extension: " + CutFileExtension(childFile.getName()));
+				System.out.println("file name: " + childName);
+				System.out.println("file absolute: " + childAbsolutePath);
+
+				fileInformation = ChildExtension(childName);
+				System.out.println("file pureName: " + fileInformation[0]);
+				System.out.println("file extension: " + fileInformation[1]);
 				System.out.println("=============================");
+
+				if (CheckFilesNameInteger(fileInformation[0])) {
+					imgCheck++;
+				}
 				childCount++;
 			} else if (childFile.isDirectory()) {
-				System.out.println("dir name: " + childFile.getName());
-				System.out.println("dir abolute: " + childFile.getAbsolutePath());
+				System.out.println("dir name: " + childName);
+				System.out.println("dir abolute: " + childAbsolutePath);
 				System.out.println("=============================");
+				isLoop = true;
+			}
+
+			if (isLoop) {
+
 				try {
-					pathFind(childFile.getAbsolutePath().toString());
+					pathFind(childAbsolutePath);
 					parentCount++;
 				} catch (NullPointerException e) {
 					System.out.println("Oops Null point..");
-					System.out.println("dir name: " + childFile.getAbsolutePath());
+					System.out.println("dir name: " + childAbsolutePath);
 					System.out.println("This. is . null");
 				}
+
 			}
 
 		}
 
 	}
 
+	public void MakeZip(String dir) throws ZipException, IOException {
+
+		FileInputStream fis = null;
+
+		ZipArchiveOutputStream zos = null;
+
+		FileOutputStream fos = new FileOutputStream(new File("d:/hahaha.zip"));
+		zos = new ZipArchiveOutputStream(fos);
+
+		try {
+			final File[] srcFiles = new File(dir).listFiles();
+
+			if (srcFiles == null || srcFiles.length == 0) {
+				throw new NullPointerException("zip source file(s) is empty (or null).");
+			}
+
+			zos.setEncoding(Charset.defaultCharset().name());
+
+			for (int i = 0; i < srcFiles.length; i++) {
+
+				ZipArchiveEntry zipEntry = new ZipArchiveEntry((srcFiles[i]).getName());
+				zipEntry.setTime(0);
+
+				zos.putArchiveEntry(zipEntry);
+				if (!srcFiles[i].isDirectory()) {
+					IOUtils.copy(new FileInputStream(srcFiles[i]), zos);
+				}
+				zos.closeArchiveEntry();
+			}
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (fis != null) {
+				fis.close();
+			}
+			if (zos != null) {
+				zos.close();
+			}
+
+		}
+	}
+
+	public void MakeZip2(String dir) {
+
+		File Path = new File(dir);
+
+		byte[] buf = new byte[1024];
+
+		String zipPath = "d:/hahaha.zip";
+		try {
+			ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipPath), Charset.forName("UTF-8"));
+
+			for (File child : Path.listFiles()) {
+
+				FileInputStream in = new FileInputStream(child);
+
+				// Path 를 넣으면 폴더와 포함되서 압축이 되어진다.
+				// out.putNextEntry(new ZipEntry(child.getAbsolutePath()));
+
+				// 아래처럼 GetName으로 경로 없이 넣어 버리면 파일들만 압축이 된다.
+				out.putNextEntry(new ZipEntry(child.getName()));
+
+				int len;
+
+				while ((len = in.read(buf)) > 0) {
+
+					out.write(buf, 0, len);
+
+				}
+				out.closeEntry();
+				in.close();
+			}
+			out.close();
+			// MoveZipFileSha(zipPath);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void MoveZipFileSha(String zipPath) throws Exception {
+		System.out.println(zipPath);
+		String shaZip = DigestUtils.extractFileHashSHA256_2(zipPath);
+		System.out.println(shaZip);
+		File zip = new File(zipPath);
+		if (zip.exists()) {
+			zip.renameTo(new File(zip.getParentFile().getAbsolutePath() + "/" + shaZip + ".zip"));
+		}
+	}
+
+	public boolean CheckFilesNameInteger(String str) {
+
+		final String intRegex = "^[0-9]+$";
+		return str.matches(intRegex);
+
+	}
+
 	public List<String> SplitRegex(String str) {
 
 		String[] strList = str.trim().replaceAll(" ", "").split(",");
-
 		return Arrays.asList(strList);
 	}
 
-	public String CutFileExtension(String path) {
+	public String[] ChildExtension(String path) {
 		Integer pin = path.lastIndexOf(".");
-		return path.substring(pin + 1, path.length());
+		String[] strList = { path.substring(0, pin), path.substring(pin + 1, path.length()) };
+
+		return strList;
 	}
 
 	public static void main(String[] args) throws IOException {
-		// String dPat = "D:/";
 		PathFinder dd = new PathFinder();
+		// String dPat = "D:/test";
 		// dd.pathFind(dPat);
-		for (String s : dd.SplitRegex(dd.imageFiles)) {
-			System.out.println(s);
-		}
-		;
+
+		dd.MakeZip("D:/test/test001");
 
 	}
 
